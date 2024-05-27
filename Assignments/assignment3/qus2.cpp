385f248384d784aa889f02684b3387c2fa3f4c68
AJAY YADAV <ajayadav3108@gmail.com>
2024-05-26 22:26:06 -0700

    Square of number

---
+++ Assignments/assignment3/qus2.cpp
@@ -0,0 +1,11 @@
+//Write a c++ program to calculate sum of two variable
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number : ";
+    cin>>a;
+    cout<<"square is : "<<a*a;
+    return 0;
+}
