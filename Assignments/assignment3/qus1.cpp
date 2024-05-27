f985cca7efdf5a72031f055d48e5e1968067462c
AJAY YADAV <ajayadav3108@gmail.com>
2024-05-26 22:25:02 -0700

    Average of 3 int

---
+++ Assignments/assignment3/qus1.cpp
@@ -0,0 +1,11 @@
+//Write a c++ program to calculate average of 3 numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three number : ";
+    cin>>a>>b>>c;
+    cout<<"Avearge is : "<<(float)(a+b+c)/3;
+    return 0;
+}
