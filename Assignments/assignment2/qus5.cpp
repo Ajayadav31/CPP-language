f7e9d1c2679c16598804a794a92a3b43cd33693b
AJAY YADAV <ajayadav3108@gmail.com>
2024-05-26 22:19:42 -0700

    volume of cuboid

---
+++ Assignments/assignment2/qus5.cpp
@@ -0,0 +1,11 @@
+//Write a c++ program to calculate volume of a cuboid
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter side of cuboid : ";
+    cin>>a;
+    cout<<"volume of cuboid is : "<<a*a*a;
+    return 0;
+}
