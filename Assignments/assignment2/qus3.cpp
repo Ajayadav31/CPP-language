20d80215ae352f802d6be50e94c6fc5c38466dfc
AJAY YADAV <ajayadav3108@gmail.com>
2024-05-26 22:15:39 -0700

    sum of two int

---
+++ Assignments/assignment2/qus3.cpp
@@ -0,0 +1,11 @@
+//Write a c++ program to calculate sum of two variable
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter two number : ";
+    cin>>a>>b;
+    cout<<"Sum is : "<<a+b;
+    return 0;
+}
