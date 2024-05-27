eafe8a61d049a2a2bafbf99c203b0b77f19cdab3
AJAY YADAV <ajayadav3108@gmail.com>
2024-05-26 22:17:58 -0700

    Area of circle

---
+++ Assignments/assignment2/qus4.cpp
@@ -0,0 +1,11 @@
+//Write a c++ program to calculate area of circle
+#include<iostream>
+using namespace std;
+int main()
+{
+    int rad;
+    cout<<"Enter radius of circle : ";
+    cin>>rad;
+    cout<<"aaarea of circle is : "<<3.14*rad*rad;
+    return 0;
+}
