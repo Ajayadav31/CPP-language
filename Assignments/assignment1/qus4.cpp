ce6bfc52b694abe2504070eca4768b4ae5f3e044
AJAY YADAV <ajayadav3108@gmail.com>
2024-05-26 21:19:25 -0700

    LCM of two no

--- Assignments/assignment1/qus4.cpp
+++ Assignments/assignment1/qus4.cpp
@@ -6,21 +6,15 @@ int main()
     int a,b,find;
     printf("Enter two number : ");
     scanf("%d %d",&a,&b);
-        printf("lcm is : %d",lcm(a,b));
+    printf("lcm is : %d",lcm(a,b));
     return 0;
 }
 int lcm(int x,int y)
 {
     int max;
-    max=x>y?x:y;
-    while(1)
+    for(max=x>y?x:y;max<=x*y;max++)
     {
         if(max%x==0&&max%y==0)
-        {  
-            return max;    
-            break;
-        }
-        max++;
+            return max; 
     }
-    return max;
 }
