dd3052115ee7be4be022ed6379e4fbbf3658db03
AJAY YADAV <ajayadav3108@gmail.com>
2024-06-08 09:14:44 -0700

    Time class

--- Assignments/assignment8/qus2.cpp
+++ Assignments/assignment8/qus2.cpp
@@ -1,1 +1,26 @@
-//
+//Define a class Time to represent Time(like 3hr 45 min 20 sec). Declare appropriate number of instans=ce member variable and also define instance member function to set values for time and display values of time
+#include<iostream>
+using namespace std;
+class Time
+{
+    private:
+        int h,m,s;
+    public:
+        void setTime(int x,int y,int z)
+        {
+            h=x;
+            m=y;
+            s=z;
+        }
+        void displayTime()
+        {
+            cout<<h<<" hr "<<m<<" min "<<s<<" sec";
+        }
+};
+int main()
+{
+    Time t;
+    t.setTime(3,45,20);
+    t.displayTime();
+    return 0;
+}
